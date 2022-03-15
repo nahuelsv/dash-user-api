ee82d2d10960792dd1096935d38c298756cfb546
cds <nahuelsilvera.cds@gmail.com>
2022-03-15 16:31:06 -0300

    change enums format

---
+++ src/enums/validations.ts
@@ -0,0 +1,3 @@
+export enum EValidations {
+    EMAIL_FORMAT = "INVALID_EMAIL_FORMAT"
+}
--- src/functions/create-user/handler.ts
+++ src/functions/create-user/handler.ts
@@ -5,10 +5,11 @@ import schema from './schema';
 import userService from '@services/users';
 import { IUser } from '@models/user';
 import { isAValidEmail } from '@libs/validators/email';
+import { EValidations } from '@enums/validations';
 
 const register: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
   if ( !isAValidEmail(event.body.email) )
-    return formatJSONResponse({ error: 'INVALID_EMAIL_FORMAT' }, 400)
+    return formatJSONResponse({ error: EValidations.EMAIL_FORMAT }, 400)
   
   let user: IUser = { ...event.body, created_at: Date.now().toString() }
   try {
